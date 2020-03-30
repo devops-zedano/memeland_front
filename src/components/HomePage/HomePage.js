aed31a192cff71758d4a523f07645e9da54f1392
Vladislav Zedano <zedano.vladislav@gmail.com>
2020-03-30 14:56:26 -0500

    Adding a breakline between upload form and the list.

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -3,20 +3,10 @@ import MemeList from '../MemeList'
 import { withMemeFetching } from '../../hoc/withMemeFetching'
 import UploadFileForm from '../UploadFileForm/UploadFileForm'
 
-// const memes = [
-//     {
-//         url: 'https://avatars0.githubusercontent.com/u/9899860?s=60&u=5610d92cc88ad157f12a1ab46ccecb0cc02aefb2&v=4',
-//         description: 'Reysmer\'s Meme'
-//     },
-//     {
-//         url: 'https://www.udemy.com/staticx/udemy/images/v6/logo-coral.svg',
-//         description: 'Udemy meme'
-//     }
-// ]
-
 const HomePage = ({ memes }) => (
     <div>
         <UploadFileForm />
+        <br />
         <MemeList memes={memes} />
     </div>
 )

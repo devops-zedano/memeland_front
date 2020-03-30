a21a6c7ed26a889f6e9680b104868565502e3523
Vladislav Zedano <zedano.vladislav@gmail.com>
2020-03-29 21:47:46 -0500

    Fixing bug that prevented build from working.

--- src/components/getMemes/index.js
+++ src/components/getMemes/index.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import { useState, useEffect } from 'react'
 
+const URL = 'https://2vnt20y917.execute-api.us-east-1.amazonaws.com/dev/list'
+
 export default () => {
     const [memes, setMemes] = useState([]);
+
     useEffect(() => {
         fetch(URL)
             .then(response => response.json())
